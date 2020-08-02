06f2c0c1bb5881364fc3ce70e40d8587619cff95
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-08-02 22:18:01 +0700

    add button generate on widget, so user can update random number by clicking the button, with Intent putExtra & getExtra in this case we call it pendingIntent

--- app/src/main/java/com/masuwes/simplewidget/widget/RandomNumbersWidget.kt
+++ app/src/main/java/com/masuwes/simplewidget/widget/RandomNumbersWidget.kt
@@ -1,8 +1,10 @@
 package com.masuwes.simplewidget.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.masuwes.simplewidget.R
 
@@ -10,6 +12,26 @@ import com.masuwes.simplewidget.R
  * Implementation of App Widget functionality.
  */
 class RandomNumbersWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val WIDGET_CLICK = "widgetsclick"
+        private const val WIDGET_ID_EXTRA = "widget_id_extra"
+    }
+
+    private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+
+        val lastUpdate = "Random: " + NumberGenerator.generate(100) // generate random number maximum 100
+        val widgetText = context.getString(R.string.appwidget_text)
+
+        // this value will call to be a text on appwidget_text
+        val views = RemoteViews(context.packageName, R.layout.random_numbers_widget)
+        views.setTextViewText(R.id.appwidget_text, lastUpdate)
+        views.setOnClickPendingIntent(R.id.btn_click, getPendingSelfIntent(context, appWidgetId, WIDGET_CLICK))
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -28,20 +50,29 @@ class RandomNumbersWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        // this method is same with the updateAppWidget, the different is on the onClick PendingIntent
+        if (WIDGET_CLICK == intent.action) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val views = RemoteViews(context.packageName, R.layout.random_numbers_widget)
+            val lastUpdate = "Random: " + NumberGenerator.generate(100)
+            val appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0)
+            views.setTextViewText(R.id.appwidget_text, lastUpdate)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    private fun getPendingSelfIntent(context: Context, appWidgetId: Int, action: String): PendingIntent {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+        intent.putExtra(WIDGET_ID_EXTRA, appWidgetId)
+        return PendingIntent.getBroadcast(context, appWidgetId, intent, 0) // result is "pending intent get all the intent extras with 0 flag"
+    }
 }
 
-internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
 
-    val lastUpdate = "Random: " + NumberGenerator.generate(100) // generate random number maximum 100
-    val widgetText = context.getString(R.string.appwidget_text)
-
-    // this value will call to be a text on appwidget_text
-    val views = RemoteViews(context.packageName, R.layout.random_numbers_widget)
-    views.setTextViewText(R.id.appwidget_text, lastUpdate)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
-}
 
 
 
